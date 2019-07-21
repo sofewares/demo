665dbd04e8a2f5b2859c5e840ebd69d470f7eafa
sunWard2017 <zsx-125@163.com>
2019-07-21 22:42:37 +0800

    add docs

---
+++ docs/precache-manifest.52bf2aced46481d000b8155c62c8b720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4736bc6b8684d625f77289a0385bcc30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61a1f6474fd67018a5c",
+    "url": "/static/css/main.4fc1aa1c.chunk.css"
+  },
+  {
+    "revision": "dd8d3c43ee75eff6b02f",
+    "url": "/static/js/2.633b6597.chunk.js"
+  },
+  {
+    "revision": "d61a1f6474fd67018a5c",
+    "url": "/static/js/main.9ad6799d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bdf05f515678c9befe77e0c442523a24",
+    "url": "/static/media/Rufina-Bold.bdf05f51.ttf"
+  }
+]);
