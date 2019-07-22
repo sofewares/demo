924ed281b3f65be56f161e97ae9476621a4d0684
zsx <84733918@qq.com>
2019-07-22 11:34:26 +0800

    modify mainimage width

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,8 @@
- const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports } = require('customize-cra');
+const { injectBabelPlugin } = require('react-app-rewired');
 
 module.exports = override(
    fixBabelImports('import', {
-    libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css',
   }),
